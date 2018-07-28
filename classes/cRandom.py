2a12f0a17b76612ba7f4bc1f26a565da51bb6e1e
Moritz Will <23119362+Mooswitz@users.noreply.github.com>
2018-07-28 14:39:13 +0200

    added docstring, made minor changes in code

--- classes/cRandom.py
+++ classes/cRandom.py
@@ -2,13 +2,18 @@ import random
 
 
 class cWRandom(object):
+    """Defines a Class for all the needed randomness"""
+
     def randomnessSeeder(self):
+        print "Seeding..."
+        print "done."
         # obj_list = []
         # for i in range(10):
+        # # change the value every time, when there is a function added!
         #     i = cWRandom()
         #     obj_list.append(i)
 
-        # for j in range(10):
+        # for j in range(10): # add every funktion from below.
         #     obj_list[j].terrainSeed(random.randrange(1, 10000, 1), j)
 
         return
