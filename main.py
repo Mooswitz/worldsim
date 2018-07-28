01627d81a11543997ee58e421a7fc06be5fafa3f
Moritz Will <23119362+Mooswitz@users.noreply.github.com>
2018-07-28 14:38:14 +0200

    made changes to docstring

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from classes.cRandom import cWRandom
 from classes.cWorld import cWorld
 """This project is work-in-progess and will run
 a very long time in development.
-However... Ideas appreciated!
-I will not explain too much to this project,
-because I want to Identify myself with this simulation."""
+"""
 
 
 def main():
-    """test"""
+    """This is the main-function for the simulation"""
     obj_earth = cWorld()
     obj_earth.fContinents(7)
 
